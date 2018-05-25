2ef65b8e33a059183bf3478ab2bdfa387b0954f5
Justin Dang <dangjustintime@gmail.com>
2018-05-25 15:38:57 -0400

    added objects to script

--- js/main.js
+++ js/main.js
@@ -1,4 +1,26 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+    {
+        rank: "queen",
+        suit: "hearts",
+        cardImage: "images/queen-of-hearts.png"
+    },
+    {
+        rank: "queen",
+        suit: "diamonds",
+        cardImage: "images/queen-of-diamonds.png"
+    },
+    {
+        rank: "king",
+        suit: "hearts",
+        cardImage: "images/king-of-hearts.png"
+    },
+    {
+        rank: "king",
+        suit: "diamonds",
+        cardImage: "images/king-of-diamonds.png"
+    },
+
+];
 var cardsInPlay = [];
 var checkForMatch = function() {
     if (cardsInPlay.length === 2) {
@@ -11,8 +33,9 @@ var checkForMatch = function() {
 };
 
 var flipCard = function(cardId) {
-    cardsInPlay.push(cards[cardId]);
-    console.log("User flipped " + cards[cardId]);
+    cardsInPlay.push(cards[cardId].rank);
+    console.log("User flipped " + cards[cardId].rank + " of " + cards[cardId].suit);
+    console.log(cards[cardId].cardImage);
     checkForMatch();
 };
 
